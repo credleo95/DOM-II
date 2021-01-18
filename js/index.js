4fde7dbcfbfed966fc2508ec1bfdf5f75d4a7cef
Omarius Credle <ocredle@omariuss-mbp.home>
2021-01-18 00:05:25 -0500

    Added dblclick event to subheadings

--- js/index.js
+++ js/index.js
@@ -4,6 +4,8 @@
 // Add color in sign me up buttons when clicked
 //
 
+
+
 const links = document.querySelectorAll("a")
 console.log(links);
 links[0].addEventListener("mouseover", event => event.target.style.color = "orangeRed")
@@ -11,6 +13,27 @@ links[1].addEventListener("mouseover", event => event.target.style.transform = "
 links[2].addEventListener("mouseover", event => event.target.style.transform = "rotate(45deg)")
 links[3].addEventListener("mouseover", event => event.target.style.transform = "rotate(50deg)")
 
+const subheadings = document.querySelectorAll("h2")
+subheadings[0].addEventListener("dblclick",event => {
+    event.target.style.transform = "scaleY(2)"
+    event.target.style.color = "red"
+    event.target.style.fontSize = "4rem"
+})
+subheadings[1].addEventListener("dblclick",event => {
+    event.target.style.transform = "scaleY(3)"
+    event.target.style.color = "blue"
+    event.target.style.fontSize = "6rem"
+})
+subheadings[2].addEventListener("dblclick",event => {
+    event.target.style.transform = "scaleY(2.4)"
+    event.target.style.color = "slateGrey"
+    event.target.style.fontSize = "2rem"
+})
+subheadings[3].addEventListener("dblclick",event => {
+    event.target.style.transform = "scaleY(2.8)"
+    event.target.style.color = "green"
+    event.target.style.fontSize = "3rem"
+})
 
 
 
